772afc6cddcb9e4691ffdf55d0d8757cf459aae5
Forrest England <forrestengland1983@gmail.com>
2026-08-20 16:47:39 -0400

    separate AudioPlayer component working

--- client/src/AudioPlayer.tsx
+++ client/src/AudioPlayer.tsx
@@ -10,30 +10,31 @@ export default function AudioPlayer({ src, isPlaying, onPlayStateChanged }: Audi
 
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
+  //  const audioCtx = new AudioContext();
+
   const playingEnded = () => {
     onPlayStateChanged(false);
   };
 
-  /*    useEffect(() => {
-      if (audioRef.current) {
-	// start playing if requested
-	if (isPlaying) {
-	  audioRef.current.load(); // Forces the player to load the new track
-	  audioRef.current.play().catch((err) => {
-            console.log("Playback interrupted or blocked by browser:", err);
-	  });
-	}
-	}, [isPlaying]); // Triggers every time this state changes */
+  // start playing when a new file is loaded - works
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.load(); // Forces the player to load the new track
+      audioRef.current.play().catch((err) => {
+        console.log("Playback interrupted or blocked by browser:", err);
+      });
+    } 
+  }, [src]); // Triggers every time this state changes 
 
-      useEffect(() => {
-	if (audioRef.current) {
-	  audioRef.current.load(); // Forces the player to load the new track
-	  audioRef.current.play().catch((err) => {
-            console.log("Playback interrupted or blocked by browser:", err);
-	  });
-	} 
-      }, [src]); // Triggers every time this state changes 
-
+  // start playing when requested - only works first time, blocked after user presses pause
+  useEffect(() => {
+    if (audioRef.current && isPlaying) {
+      audioRef.current.load(); // Forces the player to load the new track
+      audioRef.current.play().catch((err) => {
+        console.log("Playback interrupted or blocked by browser:", err);
+      });
+    } 
+  }, [isPlaying]); // Triggers every time this state changes 
 
   return (
 
